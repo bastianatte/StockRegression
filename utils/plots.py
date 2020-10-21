c28b3f3bcf754664b0a624bfae817a57ba57c478
Sebastiano Spinali <sebastiano.spinali@cern.ch>
2020-10-21 13:07:36 +0200

    Update lr plot

--- utils/plots.py
+++ utils/plots.py
@@ -1,38 +1,41 @@
-#creating a function to plot the graph and show the test result:
-def check_stationarity(y, lags_plots=48, figsize=(22,8)):
-    import pandas as pd
-    import statsmodels.api as sm
-    from statsmodels.tsa.stattools import adfuller
-    from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-    import seaborn as sns
-    from math import sqrt
-    import matplotlib.pyplot as plt
-    "Use Series as parameter"
-    
-    y = pd.Series(y)
-    fig = plt.figure()
+import matplotlib.pyplot as plt
+from utils.misc import get_logger
+import os
+import logging
 
-    ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=2)
-    ax2 = plt.subplot2grid((3, 3), (1, 0))
-    ax3 = plt.subplot2grid((3, 3), (1, 1))
-    ax4 = plt.subplot2grid((3, 3), (2, 0), colspan=2)
+plot_log = get_logger(__name__)
+plot_log.setLevel(logging.INFO)
 
-    y.plot(ax=ax1, figsize=figsize, color='teal')
-    ax1.set_title('Crystal Sugar Price')
-    plot_acf(y, lags=lags_plots, zero=False, ax=ax2, color='teal');
-    plot_pacf(y, lags=lags_plots, zero=False, ax=ax3, method='ols', color='teal');
-    sns.displot(y, bins=int(sqrt(len(y))), ax=ax4, color='teal')
-    ax4.set_title('Price Distribution')
+def linear_regression_plot(y_test, train_pred, out_path):
+    plot_log.info("in linear regression plot")
+    plt.plot(y_test.values, color='r', linewidth=1, label='test')
+    plt.plot(train_pred, '-.', color="b", linewidth=1, label='pred')
+    plt.ylabel('\u0394 Rel')
+    plt.legend(loc='upper center', frameon=False)
+    plt.title('LinearRegression')
+    figname = os.path.join(out_path, "linearRegr_plot"+".png")
+    plt.savefig(figname, dpi=200)
+    plt.close()
 
-    plt.tight_layout()
-    
-    print('Dickey-Fuller test results:')
-    adfinput = adfuller(y)
-    adftest = pd.Series(adfinput[0:4], index=['Statistical Test','P-Value','Used Lags','Observations Number'])
-    adftest = round(adftest,4)
-    
-    for key, value in adfinput[4].items():
-        adftest["Critical Values (%s)"%key] = value.round(4)
-        
-    print(adftest)
-    
+def linear_regression_scatter(y_test, train_pred, out_path):
+    plot_log.info("in linear regression scatter")
+    plt.scatter(y_test.values, train_pred)
+    plt.xlabel('\u0394 Rel')
+    plt.ylabel('\u0394 Rel predicted')
+    plt.title('\u0394 Rel comparison')
+    plt.xlim(-0.2, 0.2)
+    plt.ylim(-0.2, 0.2)
+    figname = os.path.join(out_path, "linearRegr_scatter"+".png")
+    plt.savefig(figname, dpi=200)
+    plt.close()
+
+def plot_sign(y_test, pred, out_path):
+    plot_log.info("in plot sign")
+    import numpy as np
+    plt.scatter(np.sign(y_test), np.sign(pred))
+    plt.xlabel('\u0394 Rel sign')
+    plt.ylabel('\u0394 Rel predicted sign')
+    plt.title('\u0394 Rel SIGN comparison')
+    figname = os.path.join(out_path, "linearRegr_sign"+".png")
+    plt.savefig(figname, dpi=200)
+    plt.close()
