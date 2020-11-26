f3808d8fb28a170d824508622d6e15e42202137f
Sebastiano Spinali <sebastiano.spinali@cern.ch>
2020-11-26 10:44:24 +0100

    Create misc functions

--- utils/misc.py
+++ utils/misc.py
@@ -41,39 +41,50 @@ def load_single_csv(input_csv, test_days):
     df_mod = df.loc[:, ('Date', 'Close', 'Open', 'Volume','High', 'Low')]
     df_mod.sort_values('Date')
     df_mod.set_index('Date', inplace=True)
-    df_mod.loc[:,'Clop_norm'] = (
+    df_mod.loc[:,'actual_day_rt'] = (
         df_mod.loc[:, 'Close'].subtract(df_mod.loc[:, 'Open'])).div(df_mod.loc[:, 'Open']
         )
     utils_log.info("before nan: {}".format(df_mod.shape))
     df_mod.dropna(inplace=True)
     utils_log.info("after nan: {}".format(df_mod.shape))
-    train = df_mod.iloc[:-test_days].copy()
-    test = df_mod.iloc[-test_days:].copy()
-    return df_mod, train, test
 
-def split_df(train, test):
+    df_train = df_mod.loc['2005-01-01':'2015-01-01']
+    df_test = df_mod.loc['2015-02-01':'2018-02-01']
+
+    # train = df_mod.iloc[:-test_days].copy()
+    # test = df_mod.iloc[-test_days:].copy()
+    return df_mod, df_train, df_test
+
+def split_df(df_full, train, test):
     """
     Splitting train and test dataframes in X and y.
     """
     # X_train = train.loc[:, ('Close', 'Open', 'Volume', 'High', 'Low')]
+    # X_train = train.loc[:, ('Volume', 'High', 'Low')]
     X_train = train.loc[:, ('Close', 'Open')]
-    y_train = train.loc[:, 'Clop_norm'].shift(periods=1, fill_value=0)
+    y_train = train.loc[:, 'actual_day_rt'].shift(periods=-1, fill_value=0)
     # X_test = test.loc[:, ('Close', 'Open', 'Volume', 'High', 'Low')]
+    # X_test = test.loc[:, ('Volume', 'High', 'Low')]
     X_test = test.loc[:, ('Close', 'Open')]
-    y_test = test.loc[:, 'Clop_norm']
+    y_test = test.loc[:, 'actual_day_rt'].shift(periods=-1, fill_value=0)
+
+    df_check = test.copy()
+    df_check['actual_next_day_rt'] = df_check.loc[:, 'actual_day_rt'].shift(periods=-1, fill_value=0)
+
     utils_log.info("shapes are: {}, {}, {}, {}".format(
         X_train.shape, y_train.shape, X_test.shape, y_test.shape
         ))
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train, X_test, y_test, df_check
 
-def building_df(y_test, next_day_return, str_stock):
-    ndr = pd.Series(next_day_return, index=y_test.index, name='next_day_rt')
-    # print(ndr)
+def building_df(y_test, next_day_return, str_stock, df_check):
+    ndr = pd.Series(next_day_return, index=y_test.index, name='pred_next_day_rt')
     stocks_name = pd.Series(str_stock, index=y_test.index, name='ticker')
     result = pd.concat([y_test, ndr, stocks_name], axis=1, sort=False)
     result.reset_index(inplace=True)
-    utils_log.info("df columns are: {}".format(result.columns))
-    # print(result)
+
+    ## for testing purpose ###
+    test = result.set_index(y_test.index)
+    df_check[['pred_next_day_rt','ticker']]=result[['pred_next_day_rt', 'ticker']].to_numpy()
     return result
 
 def calculate_sign(y_test, pred):
@@ -99,3 +110,83 @@ def arith_mean(list, string):
     am = val_sum/(idx+1)
     utils_log.info("Arith mean {} list is: {}%".format(string, am))
 
+def define_periods(input_csv):
+    utils_log.info("in define train test period")
+    df = pd.read_csv(input_csv)
+    df_mod = df.loc[:, ('Date')]
+    df_mod_2 = df_mod.to_frame()
+    utils_log.info("before nan: {}".format(df_mod.shape))
+    df_mod.dropna(inplace=True)
+    utils_log.info("after nan: {}".format(df_mod.shape))
+    return df_mod_2
+
+def hist_frequency_dates(df, df1, df2, out_path):
+    import matplotlib.pyplot as plt
+    import os
+    import pandas as pd
+    from datetime import datetime
+    utils_log.info("in plot hist_frequency_dates")
+    fig, ax = plt.subplots()
+    df1.reset_index(inplace=True)
+    df2.reset_index(inplace=True)
+    df["Date"] = df["Date"].astype("datetime64")
+    df = pd.to_datetime(df["Date"])
+    df1["Date"] = df1["Date"].astype("datetime64")
+    df1 = pd.to_datetime(df1["Date"])
+    df2["Date"] = df2["Date"].astype("datetime64")
+    df2 = pd.to_datetime(df2["Date"])
+    plt.hist(df, 1000, alpha=0.5, label='df full')
+    plt.hist(df1, 1000, alpha=0.5, label='df train')
+    plt.hist(df2, 1000, alpha=0.5, label='df test')
+    ax.legend(loc='upper left', frameon=False)
+    figname = os.path.join(out_path, "Date_periods" + ".png")
+    plt.savefig(figname, dpi=200)
+    plt.close()
+    utils_log.info("plot hist_frequency_dates DONE!")
+
+def create_folders(OUPUT_PATH):
+    import os
+    if not os.path.exists(OUPUT_PATH):
+        os.makedirs(OUPUT_PATH)
+    STOCKS_DIR = os.path.join(OUPUT_PATH, "Stocks")
+    if not os.path.exists(STOCKS_DIR):
+        os.makedirs(STOCKS_DIR)
+    RANK_DIR = os.path.join(OUPUT_PATH, "Ranking")
+    if not os.path.exists(RANK_DIR):
+        os.makedirs(RANK_DIR)
+    TABLE_DIR = os.path.join(RANK_DIR, "table")
+    if not os.path.exists(TABLE_DIR):
+        os.makedirs(TABLE_DIR)
+    MISC_DIR = os.path.join(OUPUT_PATH, "misc")
+    if not os.path.exists(MISC_DIR):
+        os.makedirs(MISC_DIR)
+    return STOCKS_DIR, RANK_DIR, TABLE_DIR, MISC_DIR
+
+def create_each_stock_folder(arg_input, csv_file, STOCKS_DIR):
+    import os
+    path =str(arg_input)
+    csv = os.path.join(path, csv_file)
+    csv_folder_string = str(csv_file.strip(".csv"))
+    CSV_DIR = os.path.join(STOCKS_DIR, csv_folder_string)
+    if not os.path.exists(CSV_DIR):
+        os.makedirs(CSV_DIR)
+    return csv, CSV_DIR, csv_folder_string
+
+def print_infos(cnfs):
+    """
+    Printing final general infos
+    """
+    if(cnfs["exe_lr"]):
+        arith_mean(cnfs["lr_ss_list"], "lr same sign")
+        arith_mean(cnfs["lr_os_list"], "lr opposite sign")
+    if(cnfs["exe_rf"]):
+        arith_mean(cnfs["rf_ss_list"], "rf same sign")
+        arith_mean(cnfs["rf_os_list"], "rf opposite sign")
+    if(len(cnfs['bad_df_list'])<=5):
+        utils_log.info("less than 5 df with less than 1k entries")
+    else:
+        utils_log.info("{} dfs with less than 1K entries".format(len(cnfs['bad_df_list'])))
+        for i, j in zip(range(len(cnfs['bad_df_list'])), range(len(cnfs['bad_df_shape_list']))):
+            utils_log.info("BAD DFs: {}, with shape: {}".format(
+                cnfs['bad_df_list'][i],
+                cnfs['bad_df_shape_list'][j]))
